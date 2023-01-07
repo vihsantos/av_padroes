9812243486ad7bc532cf26e93e6b8e1ebf85a38c
Vitoria <vick_santos_vick@hotmail.com>
2023-01-06 21:27:32 -0300

    #1 - Criando o relatório resumido

---
+++ src/template_method/RelatorioResumido.java
@@ -0,0 +1,35 @@
+package template_method;
+
+import java.util.List;
+
+import bridge.Meio;
+import modelos.Obra;
+
+public class RelatorioResumido extends TemplateRelatorio{
+
+    public RelatorioResumido(List<Obra> obras, Meio meio) {
+        super(obras, meio);
+    }
+
+    @Override
+    public String gerarHeader() {
+        return meio.gerarHeader();
+    }
+
+    @Override
+    public String gerarConteudo() {
+        String conteudo = "";
+        for (Obra obra : this.obras) {
+            this.meio.gerarComentario(obra.getTitulo());
+            this.meio.gerarNegrito(obra.getTitulo());
+            this.meio.gerarItalico(new String[]{"", ""});
+        }
+        return conteudo;
+    }
+
+    @Override
+    public String gerarFooter() {
+        return meio.gerarFooter();
+    }
+     
+}
