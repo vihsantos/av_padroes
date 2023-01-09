aa01863d2f50abdd9649b535b748689ca2f0a7c5
Vitoria <vick_santos_vick@hotmail.com>
2023-01-09 17:40:43 -0300

    #1 - Aplicando testes

--- src/App.java
+++ src/App.java
@@ -1,10 +1,35 @@
+import java.util.List;
+import bridge.MeioLatex;
+import modelos.Obra;
 import modelos.ObraDB;
+import template_method.RelatorioCompleto;
+import template_method.TemplateRelatorio;
+import visitor.JsonExportVisitor;
+import visitor.XMLExportVisitor;
 
 public class App {
 
     public void runApp(){
         ObraDB datasource = ObraDB.instance();
-        datasource.getObras().forEach(item->System.out.println(item.toString()));
+        List<Obra> obras = datasource.getObras();
+        JsonExportVisitor visitor = new JsonExportVisitor();
+        XMLExportVisitor visitor2 = new XMLExportVisitor();
+
+        // obras.forEach(item->{
+        //     item.accept(visitor);
+        //     System.out.println(visitor.getJson() + "\n");
+        // });
+
+        obras.forEach(item->{
+            item.accept(visitor2);
+            System.out.println(visitor2.getXML() + "\n");
+        });
+    }
+
+    public void runApp2(){
+        ObraDB datasource = ObraDB.instance();
+        TemplateRelatorio relatorio = new RelatorioCompleto(datasource.getObras(), new MeioLatex());
+        relatorio.gerarRelatorio();
     }
 
     public static void main(String[] args) throws Exception {
